a98494e2a3c13ce3947e51f08ff6654b2fcce03c
D O <s6301072510231@email.kmutnb.ac.th>
2022-10-04 01:14:58 +0700

    Sucess read data form SV

---
+++ callcur2.js
@@ -0,0 +1,121 @@
+
+    function flowtransducer(){
+        fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=flow-transducer')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // let list = document.querySelector('#list');
+            var id = document.querySelector('#idflow').value;
+            var sensor_name = data[id].name;
+            var sensor_type= data[id].type;
+            var sensor_unit = data[id].unit;
+            var sensor_range = data[id].range;
+            var sensor_plantsite = data[id]["plant-site"];
+            var sensor_value = data[id].value;
+            document.querySelector('#fname').innerHTML = `Name : ${sensor_name}.`;
+            document.querySelector('#ftype').innerHTML = `Type : ${sensor_type}.`;
+            document.querySelector('#funit').innerHTML = `unit : ${sensor_unit}.`;
+            document.querySelector('#frange').innerHTML = `Range : ${sensor_range}.`;
+            document.querySelector('#fplant-site').innerHTML = `Plant-Site : ${sensor_plantsite}.`;
+            document.querySelector('#fvalue').innerHTML = `Value : ${sensor_value}.`;
+        })  
+        .catch(error => {
+            console.log('Error:', error);
+        });
+
+        return false;
+    }
+
+    function leveltransducer(){
+        fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=level-transducer')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // let list = document.querySelector('#list');
+            var id = document.querySelector('#idlevel').value;
+            var sensor_name = data[id].name;
+            var sensor_type= data[id].type;
+            var sensor_unit = data[id].unit;
+            var sensor_range = data[id].range;
+            var sensor_plantsite = data[id]["plant-site"];
+            var sensor_value = data[id].value;
+            document.querySelector('#lname').innerHTML = `Name : ${sensor_name}.`;
+            document.querySelector('#ltype').innerHTML = `Type : ${sensor_type}.`;
+            document.querySelector('#lunit').innerHTML = `unit : ${sensor_unit}.`;
+            document.querySelector('#lrange').innerHTML = `Range : ${sensor_range}.`;
+            document.querySelector('#lplant-site').innerHTML = `Plant-Site : ${sensor_plantsite}.`;
+            document.querySelector('#lvalue').innerHTML = `Value : ${sensor_value}.`;
+        })  
+        .catch(error => {
+            console.log('Error:', error);
+        });
+
+        return false;
+    }
+
+    function temperaturetransducer(){
+        fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=temperature-transducer')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // let list = document.querySelector('#list');
+            var id = document.querySelector('#idtemp').value;
+            var sensor_name = data[id].name;
+            var sensor_type= data[id].type;
+            var sensor_unit = data[id].unit;
+            var sensor_range = data[id].range;
+            var sensor_plantsite = data[id]["plant-site"];
+            var sensor_value = data[id].value;
+            document.querySelector('#tname').innerHTML = `Name : ${sensor_name}.`;
+            document.querySelector('#ttype').innerHTML = `Type : ${sensor_type}.`;
+            document.querySelector('#tunit').innerHTML = `unit : ${sensor_unit}.`;
+            document.querySelector('#trange').innerHTML = `Range : ${sensor_range}.`;
+            document.querySelector('#tplant-site').innerHTML = `Plant-Site : ${sensor_plantsite}.`;
+            document.querySelector('#tvalue').innerHTML = `Value : ${sensor_value}.`;
+        })  
+        .catch(error => {
+            console.log('Error:', error);
+        });
+
+        return false;
+    }
+
+    function pressuretransducer(){
+        fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=pressure-transducer')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // let list = document.querySelector('#list');
+            var id = document.querySelector('#idpress').value;
+            var sensor_name = data[id].name;
+            var sensor_type= data[id].type;
+            var sensor_unit = data[id].unit;
+            var sensor_range = data[id].range;
+            var sensor_plantsite = data[id]["plant-site"];
+            var sensor_value = data[id].value;
+            document.querySelector('#pname').innerHTML = `Name : ${sensor_name}.`;
+            document.querySelector('#ptype').innerHTML = `Type : ${sensor_type}.`;
+            document.querySelector('#punit').innerHTML = `unit : ${sensor_unit}.`;
+            document.querySelector('#prange').innerHTML = `Range : ${sensor_range}.`;
+            document.querySelector('#pplant-site').innerHTML = `Plant-Site : ${sensor_plantsite}.`;
+            document.querySelector('#pvalue').innerHTML = `Value : ${sensor_value}.`;
+        })  
+        .catch(error => {
+            console.log('Error:', error);
+        });
+
+        return false;
+    }
+
+    document.addEventListener('DOMContentLoaded', function (){
+        document.querySelector('#flowtransducer').onsubmit = flowtransducer;
+    });
+    document.addEventListener('DOMContentLoaded', function (){
+        document.querySelector('#leveltransducer').onsubmit = leveltransducer;
+    });
+    document.addEventListener('DOMContentLoaded', function (){
+        document.querySelector('#temperaturetransducer').onsubmit = temperaturetransducer;
+    });
+    document.addEventListener('DOMContentLoaded', function (){
+        document.querySelector('#pressuretransducer').onsubmit = pressuretransducer;
+    });
