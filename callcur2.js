51a9ea8ae5a5ac4b5e99ac23bc256f03e36bdddb
D O <s6301072510231@email.kmutnb.ac.th>
2022-10-04 23:49:52 +0700

    pageWorking

--- callcur2.js
+++ callcur2.js
@@ -1,4 +1,32 @@
 
+    function postdata(){
+        var st_id = document.querySelector('#loginid').value;
+        var st_name = document.querySelector('#loginname').value;
+        var sensor_name = document.querySelector('#loginsname').value;
+        var sensor_type = document.querySelector('#loginstype').value;
+        var sensor_unit = document.querySelector('#loginsunit').value;
+        var sensor_value = document.querySelector('#loginsvalue').value;
+        var datapost = {
+            "st_id": `${st_id}.`,
+            "st_name": `${st_name}.`,
+            "sensor_name": `${sensor_name}.`,
+            "sensor_type": `${sensor_type}.`,
+            "sensor_unit": `${sensor_unit}.`,
+            "sensor_value": `${sensor_value}.`
+      }
+        fetch('https://midterm-exam-010723313-2022.herokuapp.com/student_post',{
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(datapost)
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+        
+        return false;
+    }
+
     function flowtransducer(){
         fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=flow-transducer')
         .then(response => response.json())
@@ -119,3 +147,10 @@
     document.addEventListener('DOMContentLoaded', function (){
         document.querySelector('#pressuretransducer').onsubmit = pressuretransducer;
     });
+    document.addEventListener('DOMContentLoaded', function (){
+        document.querySelector('#login').onsubmit = postdata;
+    });
+   
+
+    
+
